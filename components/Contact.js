41e32a351bc36b7343e68e22abf7b22948eadab3
VeisyLawalata <145326219+VeisyLawalata@users.noreply.github.com>
2023-10-29 21:19:03 +0800

    Add files via upload

---
+++ components/Contact.js
@@ -0,0 +1,10 @@
+function Contact () {
+	return(
+		<address>
+			You can find me here:<br />
+			Universitas Klabat<be />
+			Jl.Arnold Mononutu, Airmadidi Bawah, Kec.AIrmadidi, <br />
+			Kabupaten Minahasa Utara, Sulawesi Utara 95371
+		</address>
+	);
+}
